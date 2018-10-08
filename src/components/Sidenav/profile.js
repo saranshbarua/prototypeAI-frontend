f733ad2ae670439a3b33434b9ee05c87d8501ca6
saranshbarua <saranshbarua@gmail.com>
2018-10-09 02:35:46 +0530

    add profile section on nav

--- src/components/Sidenav/profile.js
+++ src/components/Sidenav/profile.js
@@ -3,7 +3,11 @@ import React from 'react'
 const ProfileBox = (props) => {
   return (
     <div className="profile-box">
-       
+       <div className="img-container">
+         <img src="" />
+       </div>
+       <div className="profile-name ssp-400">Saransh Barua</div>
+       <div className="profile-profession ssp-400">Designer</div>
     </div>
   )
 };
