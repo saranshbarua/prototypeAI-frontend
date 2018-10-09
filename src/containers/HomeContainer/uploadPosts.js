7f2d0fbb170cf329bd8ed1bfa66778b70c47586e
saranshbarua <saranshbarua@gmail.com>
2018-10-10 02:54:11 +0530

    add upload box

--- src/containers/HomeContainer/uploadPosts.js
+++ src/containers/HomeContainer/uploadPosts.js
@@ -4,7 +4,10 @@ export default class uploadPosts extends Component {
   render() {
     return (
       <div className="upload-box">
-        
+        <textarea className="upload-textarea ssp-400" placeholder="Write something..."></textarea>
+        <div className="ub-low">
+          
+        </div>
       </div>
     )
   }
