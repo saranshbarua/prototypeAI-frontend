f2f9d27dd95f186ee9118a86a9ba70cd31f03a6e
saranshbarua <saranshbarua@gmail.com>
2018-10-10 03:44:03 +0530

    complete popular posts

--- src/containers/HomeContainer/popular.js
+++ src/containers/HomeContainer/popular.js
@@ -10,7 +10,8 @@ export default class Popular extends Component {
           </div>
           <p className="ssp-400" style={{color: 'rgb(33, 150, 243)', marginLeft: '15px', fontSize: '13px'}}>Popular</p>
           <span className="popular-title ssp-400">Zumba classes</span>
-          <p className="popular-desc ssp-400">Let your child first steps be marked in zumba. Specialized in 13 categories</p>
+          <p className="popular-desc ssp-400">Let your child first steps be marked in zumba. Specialized in 13 categories.</p>
+          <div className="pop-next-container ssp-400"><span style={{marginRight: '19px'}}>Next post</span></div>
         </div>
       </div>
     )
