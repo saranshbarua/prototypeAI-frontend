6157687ca2b8014b74c481a3da07b2a2f20a5140
saranshbarua <saranshbarua@gmail.com>
2018-10-10 03:13:14 +0530

    add upload button and bar

--- src/containers/HomeContainer/uploadPosts.js
+++ src/containers/HomeContainer/uploadPosts.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPaperclip, faLocationArrow } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faPaperclip, faLocationArrow);
 
 export default class uploadPosts extends Component {
   render() {
@@ -6,7 +11,19 @@ export default class uploadPosts extends Component {
       <div className="upload-box">
         <textarea className="upload-textarea ssp-400" placeholder="Write something..."></textarea>
         <div className="ub-low">
-          
+          <div className="upload-icons-tab">
+            <FontAwesomeIcon 
+              icon="paperclip"
+              color="#445a64"
+              size="sm"
+            />
+            <FontAwesomeIcon 
+              icon="location-arrow"
+              color="#445a64"
+              size="sm"
+            />
+          </div>
+          <button className="upload-button ssp-400">Upload</button> 
         </div>
       </div>
     )
