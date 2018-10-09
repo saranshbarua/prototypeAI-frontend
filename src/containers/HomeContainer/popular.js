72361738ced729ef9051ed67510417fdd99cbb3b
saranshbarua <saranshbarua@gmail.com>
2018-10-10 03:37:07 +0530

    add popular posts UI

--- src/containers/HomeContainer/popular.js
+++ src/containers/HomeContainer/popular.js
@@ -4,7 +4,14 @@ export default class Popular extends Component {
   render() {
     return (
       <div className="popular-back">
-        <div className="popular-container"></div>
+        <div className="popular-container">
+          <div className="popular-img">
+            {/* <img src="" alt="Popular posts"/> */}
+          </div>
+          <p className="ssp-400" style={{color: 'rgb(33, 150, 243)', marginLeft: '15px', fontSize: '13px'}}>Popular</p>
+          <span className="popular-title ssp-400">Zumba classes</span>
+          <p className="popular-desc ssp-400">Let your child first steps be marked in zumba. Specialized in 13 categories</p>
+        </div>
       </div>
     )
   }
