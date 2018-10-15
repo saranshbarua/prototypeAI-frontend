d26264b5ffcaae1d55e65fd998b864a9f367f172
saranshbarua <saranshbarua@gmail.com>
2018-10-16 00:39:37 +0530

    add first form

--- src/containers/Signup/signup.js
+++ src/containers/Signup/signup.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react'
 
-class SigupOne extends Component {
+class SignupOne extends Component {
 
   updateEmailInvoke = () => {
     this.props.someMethod();
   }
 
+  invokeSubmit = (e) => {
+    e.preventDefault();
+    this.props.submitOne();
+  }
+
   render() {
     return (
-      <div className="su-one">
-        <form onSubmit={this.nextStepInvoke}>
+      <div className="su-con">
+        <form className="form-one" onSubmit={this.invokeSubmit}>
           <input 
             type="text"
+            className="standard-input"
+            placeholder="Enter a display name"
+            required
           />
+          <input 
+            type="text"
+            className="standard-input"
+            placeholder="Enter email"
+            required
+          />
+          <input 
+            type="text"
+            className="standard-input"
+            placeholder="Choose username"
+            required
+          />
+          <input 
+            type="text"
+            className="standard-input"
+            placeholder="Choose password"
+            required
+          />
+          <input 
+            type="text"
+            className="standard-input"
+            placeholder="Re-enter password"
+            required
+          />
+          <input 
+            type="text"
+            className="standard-input"
+            placeholder="Enter address"
+            required
+          />
+          <button className="standard-button" onClick={this.invokeSubmit}>Next</button>
         </form>
-        {this.props.email}
-        <br/>
-        <button onClick={this.updateEmailInvoke}>Update email</button>
       </div>
     )
   }
@@ -25,9 +61,17 @@ class SigupOne extends Component {
 class SignupTwo extends Component {
   render() {
     return (
-      <div style={{display: 'none'}}>
+      <div>
         Component 2
       </div>
+    )
+  }
+}
+
+class SignupThree extends Component {
+  render() {
+    return (
+      <div>Component 3</div>
     )
   }
 }
@@ -55,21 +99,23 @@ export default class Signup extends Component {
     console.log(this.state.email);
   }
 
+  submitFirstPart = () => {
+    console.log('first part submitted');
+    this.setState({
+      isShowing: 2
+    })
+  }
+
   render() {
     return (
       <div className="test-form">
         <div className="signup-container">
           <p className="ssp-300" style={{fontSize: '35px', color: '#263238'}}>Signup for your account</p>
-          <div>{
-            () => {
-              switch(this.state.isShowing) {
-                case(1):
-                  return <SigupOne {...this.state} someMethod = {this.updateEmail} />;
-                case(2):
-                  return <SignupTwo />;
-              }
-            }
-          }</div>
+          <div style={{height: '95%', width: '60%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <SignupOne submitOne = {this.submitFirstPart} />
+            {/* <SignupTwo />
+            <SignupThree /> */}
+          </div>
         </div>
       </div>
     )
