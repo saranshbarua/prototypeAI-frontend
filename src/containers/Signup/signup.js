9f2a01e4ebdbf03617fd0c758126822ba1ecbb72
saranshbarua <saranshbarua@gmail.com>
2018-10-15 17:42:31 +0530

    add signup state

--- src/containers/Signup/signup.js
+++ src/containers/Signup/signup.js
@@ -3,6 +3,15 @@ import React, { Component } from 'react'
 export default class Signup extends Component {
   constructor(props) {
     super(props);
+    this.state= {
+      name: '',
+      email: '',
+      password: '',
+      username: '',
+      address: '',
+      skillset: [],
+      interests: []
+    }
   }
 
   render() {
