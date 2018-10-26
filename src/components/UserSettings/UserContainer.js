a86d1f300cb1c10740edfe8e5fce5e51d0bcedd7
saranshbarua <saranshbarua@gmail.com>
2018-10-26 16:03:11 +0530

    add user settings container

---
+++ src/components/UserSettings/UserContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UserContainer extends Component {
+  render() {
+    return (
+      <div className="user-container">
+        
+      </div>
+    )
+  }
+}
