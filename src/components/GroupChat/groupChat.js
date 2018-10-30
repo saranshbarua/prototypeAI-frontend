17e1977c63d48323cd6b2945a32741d344f45d03
saranshbarua <saranshbarua@gmail.com>
2018-10-30 20:27:51 +0530

    add groupchat input

--- src/components/GroupChat/groupChat.js
+++ src/components/GroupChat/groupChat.js
@@ -5,7 +5,13 @@ export default class groupChat extends Component {
     return (
       <div className="groupchat-container">
         <div className="gpchat-header">
-            <h3 className="ssp-400" style={{color: 'white'}}>Group name</h3>
+            <span className="ssp-400" style={{color: 'white', marginLeft: '10px', fontSize: '22px'}}>Group name</span>
+            <span className="ssp-400" style={{color: 'white', marginLeft: '10px', fontStyle: 'italic', fontSize: '16px'}}>saransh shivansh himanshu param</span>
+        </div>
+
+        <div className="gp-input-container">
+            <input type="text" className="gpchat-input"/>
+            <button className="sendbutton"></button>
         </div>
       </div>
     )
