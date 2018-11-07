dab59b46a76f736e5fe3da5e9ced9b6bdda1d02c
saranshbarua <saranshbarua@gmail.com>
2018-11-07 16:11:46 +0530

    separate top three suggestions (without sorting logic)

--- src/containers/HomeContainer/suggestions.js
+++ src/containers/HomeContainer/suggestions.js
@@ -11,73 +11,56 @@ export default class Suggestions extends Component {
     super(props);
     this.state = {
       suggestedUsers: [],
-      loggedInUser: '',
+      loggedInUser: this.props.loggedIn,
     }
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3000/user').then(response => response.json())
+    .then((result) => {
+       this.setState({
+         suggestedUsers: result.filter(x => {
+           return x.displayName !== this.state.loggedInUser
+         })
+       });
+     }
+    )
+  }
+
   render() {
+    const lengthOfUsers = this.state.suggestedUsers.length;
+    this.state.suggestedUsers.splice(3,lengthOfUsers - 3);
+    console.log(this.state.suggestedUsers);
+
+    const showTopThree = this.state.suggestedUsers.map((user, key) => {
+      return (
+        <div className="sugg-box" key = {key}>
+          <div className="sugg-avatar">
+            <img src="https://avatars0.githubusercontent.com/u/23500643?v=3&s=100" alt="" className="sugg-img" />
+          </div>
+          <div className="sugg-desc ssp-400">
+            <span style={{color: '#2196f3'}}>{ user.displayName }</span>
+            <span style={{color: '#607d8b', fontSize: '13px'}}>{ user.designation }</span>
+          </div>
+          <div className="sugg-add">
+            <button className="add-sugg">
+              <FontAwesomeIcon 
+                icon="plus"
+                size="sm"
+                color="grey"
+              />
+            </button>
+          </div>
+        </div>
+      )
+    })
+
     return (
       <div className="suggestion-container">
         <div className="sugg-title ssp-400">
             Suggested people
         </div>
-        <div className="sugg-box">
-          <div className="sugg-avatar">
-            <img src="https://avatars0.githubusercontent.com/u/23500643?v=3&s=100" alt="" className="sugg-img" />
-          </div>
-          <div className="sugg-desc ssp-400">
-            <span style={{color: '#2196f3'}}>Saransh Barua</span>
-            <span style={{color: '#607d8b', fontSize: '13px'}}>Photographer</span>
-          </div>
-          <div className="sugg-add">
-            <button className="add-sugg">
-              <FontAwesomeIcon 
-                icon="plus"
-                size="sm"
-                color="grey"
-              />
-            </button>
-          </div>
-        </div>
-
-        <div className="sugg-box">
-          <div className="sugg-avatar">
-            <img src="https://avatars0.githubusercontent.com/u/23500643?v=3&s=100" alt="" className="sugg-img" />
-          </div>
-          <div className="sugg-desc ssp-400">
-            <span style={{color: '#2196f3'}}>Param Nagar</span>
-            <span style={{color: '#607d8b', fontSize: '13px'}}>Cyber Punk</span>
-          </div>
-          <div className="sugg-add">
-            <button className="add-sugg">
-              <FontAwesomeIcon 
-                icon="plus"
-                size="sm"
-                color="grey"
-              />
-            </button>
-          </div>
-        </div>
-
-        <div className="sugg-box">
-          <div className="sugg-avatar">
-            <img src="https://avatars0.githubusercontent.com/u/23500643?v=3&s=100" alt="" className="sugg-img" />
-          </div>
-          <div className="sugg-desc ssp-400">
-            <span style={{color: '#2196f3'}}>Aditya Dimri</span>
-            <span style={{color: '#607d8b', fontSize: '13px'}}>Evangelist</span>
-          </div>
-          <div className="sugg-add">
-            <button className="add-sugg">
-              <FontAwesomeIcon 
-                icon="plus"
-                size="sm"
-                color="grey"
-              />
-            </button>
-          </div>
-        </div>
-       
+        {showTopThree}
       </div>
     )
   }
