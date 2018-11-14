a99ba93fc00bbe77473bb3d69af9b596ca5bb1de
saranshbarua <saranshbarua@gmail.com>
2018-11-14 10:24:44 +0530

    add userinfo box

--- src/components/UserProfile/userProfile.js
+++ src/components/UserProfile/userProfile.js
@@ -6,7 +6,14 @@ export default class UserProfile extends Component {
       <div className="up-container">
         <div className="user-cover">
           <div className="user-dp">
-            
+      
+          </div>
+        </div>
+
+        <div className="up-down">
+          <div className="user-posts"></div>
+          <div className="user-info">
+            <div className="dis-name ssp-300">Saransh Barua</div>
           </div>
         </div>
       </div>
