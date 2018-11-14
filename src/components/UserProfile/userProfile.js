766194f2a495293259a8cb95a83091504b8d7348
saranshbarua <saranshbarua@gmail.com>
2018-11-14 14:07:05 +0530

    add user skills and interests

--- src/components/UserProfile/userProfile.js
+++ src/components/UserProfile/userProfile.js
@@ -18,7 +18,24 @@ export default class UserProfile extends Component {
             <p className="user-bio ssp-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
-            
+            <span className="ssp-400" style={{alignSelf: 'flex-start', marginLeft: '20px'}}>Skills</span>
+            <div className="user-skills">
+              <div className="uskill ssp-400">Design</div>
+              <div className="uskill ssp-400">Cooking</div>
+              <div className="uskill ssp-400">Developing</div>
+              <div className="uskill ssp-400">Direction</div>
+            </div>
+            <span className="ssp-400" style={{alignSelf: 'flex-start', marginLeft: '20px'}}>Interests</span>
+            <div className="user-skills">
+              <div className="uskill ssp-400">Design</div>
+              <div className="uskill ssp-400">Cooking</div>
+              <div className="uskill ssp-400">Developing</div>
+              <div className="uskill ssp-400">Direction</div>
+              <div className="uskill ssp-400">Design</div>
+              <div className="uskill ssp-400">Cooking</div>
+              <div className="uskill ssp-400">Developing</div>
+              <div className="uskill ssp-400">Direction</div>
+            </div>
           </div>
         </div>
       </div>
