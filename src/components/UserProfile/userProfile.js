5e725e028091fd806dd33847e3bd1bf4255600cf
saranshbarua <saranshbarua@gmail.com>
2018-11-14 10:09:50 +0530

    add user cover photo, user profile pic UI

--- src/components/UserProfile/userProfile.js
+++ src/components/UserProfile/userProfile.js
@@ -4,7 +4,11 @@ export default class UserProfile extends Component {
   render() {
     return (
       <div className="up-container">
-        
+        <div className="user-cover">
+          <div className="user-dp">
+            
+          </div>
+        </div>
       </div>
     )
   }
