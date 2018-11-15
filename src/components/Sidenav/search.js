cc9f7e632fe6091d6f6ff7f5497784c1fb5e61a7
saranshbarua <saranshbarua@gmail.com>
2018-11-15 10:00:06 +0530

    add stateful form for search

--- src/components/Sidenav/search.js
+++ src/components/Sidenav/search.js
@@ -1,10 +1,47 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
 export default class Search extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchedValue: '',
+      redirect: false
+    }
+  }
+
+  handleChange(event) {
+    this.setState({
+      searchedValue: event.target.value
+    })
+  }
+
+  submitForm(event) {
+    event.preventDefault();
+    this.setState({
+      redirect: true
+    });
+    console.log(this.state.searchedValue)
+  }
+
   render() {
+    const redirection = this.state.redirect;
+    if(redirection) {
+      return <Redirect to={{
+        pathname: '/groupchat',
+        state: {
+          searchedFor: this.state.searchedValue
+        }
+      }} />
+    }
     return (
       <div className="search-bar">
-        <input className="search-input ssp-600" placeholder="Search" />
+        <form 
+          onChange={(e) => (this.handleChange(e))} value={this.state.searchedValue}
+          onSubmit={(e) => (this.submitForm(e))}
+        >
+          <input className="search-input ssp-600" placeholder="Search" />        
+        </form>
       </div>
     )
   }
