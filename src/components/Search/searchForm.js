41e5002df050d5d20e94da2eb1ada0214db943c4
saranshbarua <saranshbarua@gmail.com>
2018-11-15 19:05:33 +0530

    hide search route

--- src/components/Search/searchForm.js
+++ src/components/Search/searchForm.js
@@ -34,34 +34,34 @@ export default class SearchForm extends Component {
     }
   }
 
-  componentDidMount() {
-    console.log('HEykd' + this.state.searchedUser)
-    fetch(`http://localhost:3000/user?username=${this.state.searchedUser}`)
-    .then(res => res.json()).then(
-      (result) => {
-        this.setState({
-          searchedUserInfo: {
-            id: result[0].id,
-            displayName: result[0].displayName,
-            userAvatar: result[0].userAvatar,
-            username: result[0].username,
-            email: result[0].email,
-            password: result[0].password,
-            location: result[0].location,
-            network: result[0].network,
-            interests: result[0].interests,
-            skills: result[0].skills,
-            designation: result[0].designation,
-            bio: result[0].bio,
-            Address: result[0].Address
-          }
-        });
-        console.log(`The info given is ${this.state.searchedUserInfo.skills}`);
-    })
-  }
+  // componentDidMount() {
+  //   console.log('HEykd' + this.state.searchedUser)
+  //   fetch(`http://localhost:3000/user?username=${this.state.searchedUser}`)
+  //   .then(res => res.json()).then(
+  //     (result) => {
+  //       this.setState({
+  //         searchedUserInfo: {
+  //           id: result[0].id,
+  //           displayName: result[0].displayName,
+  //           userAvatar: result[0].userAvatar,
+  //           username: result[0].username,
+  //           email: result[0].email,
+  //           password: result[0].password,
+  //           location: result[0].location,
+  //           network: result[0].network,
+  //           interests: result[0].interests,
+  //           skills: result[0].skills,
+  //           designation: result[0].designation,
+  //           bio: result[0].bio,
+  //           Address: result[0].Address
+  //         }
+  //       });
+  //       console.log(`The info given is ${this.state.searchedUserInfo.skills}`);
+  //   })
+  // }
 
   render() {
-    console.log(`Props passed are ${this.state.searchedUser}`)
+    // console.log(`Props passed are ${this.state.searchedUser}`)
     return (
       <div className="up-container">
         Hey there 
